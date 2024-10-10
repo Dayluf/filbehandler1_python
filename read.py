99d8f09e5bb4721178fd70e64f38aead30d22186
elev <Daniel.nordhaug@outlook.com>
2024-10-10 12:58:02 +0200

    klarte nesten oppgave 2

--- read.py
+++ read.py
@@ -1,4 +1,6 @@
 with open("read.txt", "r") as file_object:
         content = file_object.read()
         print(content)
-        
+
+with open("write.txt", "a") as user_input:
+        content = user_input.write(" fårhåpentligvis vil ikke teksten som allerede står der bli slettet \n")
