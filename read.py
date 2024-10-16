b496273e396dfee60ff3900dbd49274cd6c9e50c
Dayluf <Daniel.nordhaug@outlook.com>
2024-10-16 10:12:26 +0200

    noe

--- read.py
+++ read.py
@@ -2,5 +2,6 @@ with open("read.txt", "r") as file_object:
         content = file_object.read()
         print(content)
 
-with open("write.txt", "a") as user_input:
-        content = user_input.write(" fårhåpentligvis vil ikke teksten som allerede står der bli slettet \n")
+with open("write.txt", "a") as f:
+        command = input("\n")
+        content = f.write(command)
